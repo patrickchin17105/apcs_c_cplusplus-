59aff0c6af8a5006c19eccbc90bf19cd2f2aebbe
patrickchin17105 <118583068+patrickchin17105@users.noreply.github.com>
2022-11-24 09:22:53 +0000

    Create A+B.cpp

---
+++ A+-*/A+B.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    
+    int a, b ;
+    
+   printf("A => " ); 
+   scanf("%d" , &a );
+   printf("B =>  "); 
+   scanf("%d", &b );
+   printf("A + B =%d\n" , a + b );
+   printf("A - B =%d\n" , a - b );
+   printf("A * B =%d\n" , a * b );
+   printf("A / B =%d\n" , a / b );
+   printf("A % B =%d\n" , a % b );   //mode
+  return 0;
+}
