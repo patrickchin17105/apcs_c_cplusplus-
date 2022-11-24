471050f08dccd3df307b2f34a551d1a18c7500a9
patrickchin17105 <118583068+patrickchin17105@users.noreply.github.com>
2022-11-24 14:49:35 +0000

    Update bool_value.cpp

--- bool_value.cpp
+++ bool_value.cpp
@@ -11,4 +11,6 @@ int main(void) {
    printf(" x > y ==> %d \n ",bool_value) ;
    bool_value = ( x < y );
    printf(" x < y ==> %d \n ",bool_value );
+   bool_value = (x < 5 && x < 10 );
+   printf(" x < 5 && x < 10 ==> %d \n ", bool_value);
 }
