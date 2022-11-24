b88f3af3f0af2bb3bd392c171690b3cad80efdb6
patrickchin17105 <118583068+patrickchin17105@users.noreply.github.com>
2022-11-24 15:38:14 +0000

    Update if_a>b_else_if_a<b.cpp

--- if_a>b_else_if_a<b.cpp
+++ if_a>b_else_if_a<b.cpp
@@ -2,22 +2,25 @@
 
 using namespace std;
 
-int main()
+int
+main ()
 {
-    int a, b ;
-    printf("please input a number : \n");
-    scanf("%d" , &a);
-    printf("please input another number : \n");
-    scanf("%d" , &b);
-    
-    if(a > b){
-        printf("a > b \n");
-    }
-    
-    else if(a < b){
-        printf("a < b \n");
-    }
-    cout<<"Hello World";
+  int a, b;
+  printf ("please input a number : \n");
+  scanf ("%d", &a);
+  printf ("please input another number : \n");
+  scanf ("%d", &b);
 
-    return 0;
+  if (a > b)
+    {
+      printf ("a > b \n");
+    }
+
+  else if (a < b)
+    {
+      printf ("a < b \n");
+    }
+  cout << "Hello World";
+
+  return 0;
 }
