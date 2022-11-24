9747c7e84d1c7dc4b90e4008254da794f200a401
patrickchin17105 <118583068+patrickchin17105@users.noreply.github.com>
2022-11-24 15:12:01 +0000

    Create if_a>b_else_if_a<b.cpp

---
+++ if_a>b_else_if_a<b.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    
+    int a , b ;
+    printf(" Please input a number : \n");
+    scanf ("%d" , &a);
+    printf("Please input another number : \n");
+    scanf("%d" , &b);
+    
+    if ( a > b) {
+        printf("a > b \n");
+    }
+    else if (a < b ){
+        printf("a < b \n");
+    }
+}
