a409de02f2081d2f612a2e9087b219d4b0dbe5d0
patrickchin17105 <118583068+patrickchin17105@users.noreply.github.com>
2022-12-03 08:26:09 +0000

    Create double_for_loop.cpp

---
+++ double_for_loop.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  int i;
+  for(i=1;i<10;i++){
+    for(j=1;j<10;j++){
+      printf("%d * %d = %d \n",i,j,i*j);
+    }
+  }
+  cont<<"Hello World";
+  
+  retrun 0;
+}
