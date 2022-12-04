6047412228fc812576137c8b6947572539ddcb6c
patrickchin17105 <118583068+patrickchin17105@users.noreply.github.com>
2022-12-04 03:01:11 +0000

    Update function_pointer.cpp

--- function_pointer.cpp
+++ function_pointer.cpp
@@ -1,11 +1,11 @@
 #include <stdio.h>
 int add(int x, int y){
-  retrun x+y;
+  return x+y;
 }
 int main(){
   int (*lp_fun)(int, int);
   lp_fun=add;
   
   printf("3 + 4 = %d \n",lp_fun(3,4));
-  retrun 0;
+  return 0;
 }  
