b4c09e8a4b626c7439eb3eb0c0ad4cd9509bf7b4
patrickchin17105 <118583068+patrickchin17105@users.noreply.github.com>
2022-12-04 06:58:33 +0000

    Create class_length_breadtg_height.cpp

---
+++ class_length_breadtg_height.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Room {
+  private:
+    double length;
+    double breadth;
+    double height;
+  public:
+    //function to initialize private variables
+    void getData(double len, double brth, double hgt) {
+      length = len;
+      breagth = brth;
+      height = hgt;
+      待續
